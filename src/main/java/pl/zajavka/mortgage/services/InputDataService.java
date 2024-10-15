b689f8b243a0e4033b2cbab8618cbfeeecbfdc27
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-10-15 09:33:39 +0200

    added inputDataService.java class

--- src/main/java/pl/zajavka/mortgage/services/InputDataService.java
+++ src/main/java/pl/zajavka/mortgage/services/InputDataService.java
@@ -1,5 +1,22 @@
 package pl.zajavka.mortgage.services;
 
+import pl.zajavka.mortgage.model.InputData;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 public class InputDataService {
 
+    private static final Path FILE_LOCATION = Paths.get("src/main/resources/inputData.csv");
+
+    public InputData read() throws IOException {
+        var content = Files.readString(FILE_LOCATION)
+                .lines()
+                .collect(Collectors.groupingBy(line -> line.split(";")[0]));
+
+        return null;
+    }
 }
